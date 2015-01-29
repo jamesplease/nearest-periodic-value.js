e23c4e48742d1531e02ecdc1b5ecbf08525558e6
Jmeas <jellyes2@gmail.com>
2015-01-29 12:00:34 -0500

    v1.0.0

--- src/nearest-periodic-value.js
+++ src/nearest-periodic-value.js
@@ -1,11 +1,8 @@
 function nearestPeriodicValue(point, value, period) {
-  // The distance between the value and the point
-  var diff = value - point;
-
   // Subtract any excess from our value, which is
-  // given the period times the number of periods
+  // given by the period times the number of periods
   // in the diff. parseInt drops our decimal values.
-  return value - period * parseInt(diff/period);
+  return value - period * parseInt((value - point)/period);
 }
 
 export default nearestPeriodicValue;
