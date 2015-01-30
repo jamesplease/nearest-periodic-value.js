9491faaab517b1f9d375a9cf4070083758807d4e
Jmeas <jellyes2@gmail.com>
2015-01-30 14:56:36 -0500

    v1.1.0

--- dist/nearest-periodic-value.js
+++ dist/nearest-periodic-value.js
@@ -9,11 +9,24 @@
 })(this, function () {
   "use strict";
 
+  /*jshint -W018 */
+
   var nearestPeriodicValue = function (point, value, period) {
+    var relation = (value - point) / period;
+
+    // We're equidistant from the nearest point if the
+    // distance from the point is a half-integer value
+    // of our period.
+    var equidistant = !(relation % 0.5) && relation % 1;
+
+    // If we're equidistant, then we add a period to
+    // ensure that we always pick the value in front
+    var mod = equidistant ? period : 0;
+
     // Adjust our value by an amount given by the closest #
     // of periods contained in the distance between the point
     // and the value
-    return value - period * Math.round((value - point) / period);
+    return mod + (value - period * Math.round(relation));
   };
 
 
